57b9b0f5c36be4312c2bfe7c2cc8511f06d79b7c
Arnav Pundir <arnavp128@gmail.com>
2025-10-30 21:48:20 +0530

    Fix syntax error in app.run() call

--- server.py
+++ server.py
@@ -122,4 +122,4 @@ def send_booking():
 # -------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True
+    app.run(host="0.0.0.0", port=port, debug=True)
